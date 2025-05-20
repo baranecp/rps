8b5c238a1698352aa9d134bd7f28070dbbd0b552
baranecp <baranec.dev@gmail.com>
2025-05-20 12:52:16 +0200

    Add function to get computer choice

---
+++ script.js
@@ -0,0 +1,17 @@
+   // Create a function called getComputerChoice
+     // Inside the function, create a variable to store a random number between 0 and 2
+     // If the number is 0, the computer chooses Rock
+     // If the number is 1, the computer chooses Paper
+     // If the number is 2, the computer chooses Scissors
+        function getComputerChoice() {
+            const randomNumber = Math.floor(Math.random() * 3);
+            if(randomNumber === 0) {
+                return "Rock";
+            } else if(randomNumber === 1) {
+                return "Paper";
+            } else {
+                return "Scissors";
+            }
+        }
+
+        console.log(getComputerChoice())
