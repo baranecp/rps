95efa3776db297569f8a5c943bd636c1e4004cfe
baranecp <baranec.dev@gmail.com>
2025-05-20 13:28:49 +0200

    Add function to play a single round

--- script.js
+++ script.js
@@ -6,11 +6,11 @@
         function getComputerChoice() {
             const randomNumber = Math.floor(Math.random() * 3);
             if(randomNumber === 0) {
-                return "Rock";
+                return "rock";
             } else if(randomNumber === 1) {
-                return "Paper";
+                return "paper";
             } else {
-                return "Scissors";
+                return "scissors";
             }
         }
 
@@ -21,6 +21,27 @@
             const humanChoice = prompt("Your choice: ");
             return humanChoice.toLocaleLowerCase();
         }
+        
+        let humanScore = 0;
+        let computerScore = 0;
 
 
+        function playRound(humanChoice, computerChoice) {
+            if (humanChoice === computerChoice) {
+                console.log("It's a tie!");
+            } else if(humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" ||
+                humanChoice === "scissors" && computerChoice === "rock")
+            {
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                computerScore++;
+            } else if (humanChoice === "paper" && computerChoice === "prock" || humanChoice === "scissors" && computerChoice === "paper" ||
+                humanChoice === "rock" && computerChoice === "scissors") {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
+        }
 
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();  
+
+        playRound(humanChoice, computerChoice);
